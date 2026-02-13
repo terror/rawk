0b52ccf5dc7b35ada0c6dc2036756a11a8498e90
Liam <liam@scalzulli.com>
2026-02-12 19:30:55 -0500

    Add parser test suite

--- src/parser.rs
+++ src/parser.rs
@@ -100,3 +100,162 @@ where
     .then_ignore(end())
     .map(|items| Program { items })
 }
+
+#[cfg(test)]
+mod tests {
+  use super::*;
+
+  struct Test<'src> {
+    expected: Program,
+    input: &'src str,
+  }
+
+  impl<'src> Test<'src> {
+    fn expected(self, expected: Program) -> Self {
+      Self { expected, ..self }
+    }
+
+    fn input(self, input: &'src str) -> Self {
+      Self { input, ..self }
+    }
+
+    fn new() -> Self {
+      Self {
+        expected: Program { items: Vec::new() },
+        input: "",
+      }
+    }
+
+    fn run(self) {
+      let (tokens, lex_errors) = lexer::lex(self.input);
+
+      assert!(lex_errors.is_empty(), "{lex_errors:?}");
+
+      let (program, parse_errors) = super::parse(tokens, self.input.len());
+
+      assert!(parse_errors.is_empty(), "{parse_errors:?}");
+      assert_eq!(program.unwrap(), self.expected);
+    }
+  }
+
+  #[test]
+  fn invalid_input_reports_errors() {
+    let (tokens, lex_errors) = lexer::lex("function foo(bar { baz }");
+
+    assert!(lex_errors.is_empty(), "{lex_errors:?}");
+
+    let (program, parse_errors) =
+      super::parse(tokens, "function foo(bar { baz }".len());
+
+    assert_eq!(program, None);
+
+    let actual = parse_errors
+      .into_iter()
+      .map(|error| error.to_string())
+      .collect::<Vec<_>>();
+
+    assert_eq!(actual, vec!["found '{' expected ',', or ')'".to_string()]);
+  }
+
+  #[test]
+  fn no_tokens_returns_no_errors() {
+    let (program, errors) = super::parse(None, 0);
+
+    assert_eq!(program, None);
+    assert_eq!(errors.len(), 0);
+  }
+
+  #[test]
+  fn parses_empty_program() {
+    Test::new()
+      .input("")
+      .expected(Program { items: Vec::new() })
+      .run();
+  }
+
+  #[test]
+  fn parses_function_definitions() {
+    Test::new()
+      .input("function foo() { bar } function baz(qux, bob,) { foo }")
+      .expected(Program {
+        items: vec![
+          TopLevelItem::Function(FunctionDefinition {
+            body: Block {
+              items: vec![BlockItem::TokenStub],
+            },
+            name: "foo".to_string(),
+            parameters: Vec::new(),
+          }),
+          TopLevelItem::Function(FunctionDefinition {
+            body: Block {
+              items: vec![BlockItem::TokenStub],
+            },
+            name: "baz".to_string(),
+            parameters: vec!["qux".to_string(), "bob".to_string()],
+          }),
+        ],
+      })
+      .run();
+  }
+
+  #[test]
+  fn parses_nested_blocks() {
+    Test::new()
+      .input("{ foo { bar { baz } } qux }")
+      .expected(Program {
+        items: vec![TopLevelItem::PatternAction(PatternAction {
+          action: Block {
+            items: vec![
+              BlockItem::TokenStub,
+              BlockItem::Block(Block {
+                items: vec![
+                  BlockItem::TokenStub,
+                  BlockItem::Block(Block {
+                    items: vec![BlockItem::TokenStub],
+                  }),
+                ],
+              }),
+              BlockItem::TokenStub,
+            ],
+          },
+          pattern: None,
+        })],
+      })
+      .run();
+  }
+
+  #[test]
+  fn parses_pattern_actions() {
+    Test::new()
+      .input("BEGIN { foo } END { bar } baz { qux } { bob }")
+      .expected(Program {
+        items: vec![
+          TopLevelItem::PatternAction(PatternAction {
+            action: Block {
+              items: vec![BlockItem::TokenStub],
+            },
+            pattern: Some(Pattern::Begin),
+          }),
+          TopLevelItem::PatternAction(PatternAction {
+            action: Block {
+              items: vec![BlockItem::TokenStub],
+            },
+            pattern: Some(Pattern::End),
+          }),
+          TopLevelItem::PatternAction(PatternAction {
+            action: Block {
+              items: vec![BlockItem::TokenStub],
+            },
+            pattern: Some(Pattern::ExpressionStub),
+          }),
+          TopLevelItem::PatternAction(PatternAction {
+            action: Block {
+              items: vec![BlockItem::TokenStub],
+            },
+            pattern: None,
+          }),
+        ],
+      })
+      .run();
+  }
+}
